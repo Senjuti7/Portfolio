ff83e0ef316a8f2748ead2618b34c4c10380eccd
Senjuti7 <maitrasenjuti9@gmail.com>
2026-01-09 10:08:48 +0530

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-outfit)"], // default text
+        Ovo: ["var(--font-ovo)"],     // headings
+      },
+      boxShadow: {
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
+      }
+    },
+  },
+};
